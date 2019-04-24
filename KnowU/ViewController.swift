e3885a051eee39d19ec7288a397d681b4a277be5
Sabrina Sturtevant <sabrinasturtevant@host-250-253.arjogro.jonesboro.ar.us.clients.pavlovmedia.net>
2019-04-23 21:11:23 -0500

    initial commit. barely has any functionality but I have many basic elements set up

--- KnowU/ViewController.swift
+++ KnowU/ViewController.swift
@@ -9,6 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redColorImageView: UIImageView!
+    @IBOutlet weak var orangeColorImageView: UIImageView!
+    @IBOutlet weak var yellowColorImageView: UIImageView!
+    @IBOutlet weak var greenColorImageView: UIImageView!
+    @IBOutlet weak var darkGreenColorImageView: UIImageView!
+    @IBOutlet weak var blueColorImageView: UIImageView!
+    @IBOutlet weak var darkBlueColorImageView: UIImageView!
+    @IBOutlet weak var purpleColorImageView: UIImageView!
+    @IBOutlet weak var pinkColorImageView: UIImageView!
+    @IBOutlet weak var blackColorImageView: UIImageView!
+    
+    var red = 0
+    var orange = 0
+    var yellow = 0
+    var green = 0
+    var darkgreen = 0
+    var blue = 0
+    var darkblue = 0
+    var purple = 0
+    var pink = 0
+    var black = 0
+    
+    @IBAction func redColorTapped(_ sender: Any) {
+       red += 1
+        orange = 0
+        yellow = 0
+        green = 0
+        darkgreen = 0
+        blue = 0
+        darkblue = 0
+        purple = 0
+        pink = 0
+        black = 0
+        orangeColorImageView.alpha = 0.5
+        yellowColorImageView.alpha = 0.5
+        greenColorImageView.alpha = 0.5
+        darkGreenColorImageView.alpha = 0.5
+        blueColorImageView.alpha = 0.5
+        darkBlueColorImageView.alpha = 0.5
+        purpleColorImageView.alpha = 0.5
+        pinkColorImageView.alpha = 0.5
+        blackColorImageView.alpha = 0.5
+        redColorImageView.alpha = 1.0
+        print(red)
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "nextButtonSegue", sender: nil)
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
